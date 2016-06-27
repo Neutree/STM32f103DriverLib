909bf973cd8953a8bd594c8457440e5665d74563
Neucrack <CZD666666@gmail.com>
2016-06-27 19:16:16 +0800

    offchip template

---
+++ template/MDK/offchip/stm32103c8t6/RTE/RTE_Components.h
@@ -0,0 +1,24 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'demo' 
+ * Target:  'template' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_DEVICE_STDPERIPH_ADC
+#define RTE_DEVICE_STDPERIPH_DMA
+#define RTE_DEVICE_STDPERIPH_EXTI
+#define RTE_DEVICE_STDPERIPH_FLASH
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+#define RTE_DEVICE_STDPERIPH_GPIO
+#define RTE_DEVICE_STDPERIPH_I2C
+#define RTE_DEVICE_STDPERIPH_RCC
+#define RTE_DEVICE_STDPERIPH_TIM
+#define RTE_DEVICE_STDPERIPH_USART
+
+#endif /* RTE_COMPONENTS_H */
---
+++ template/MDK/offchip/stm32103c8t6/app/Main.cpp
@@ -0,0 +1,50 @@
+#include "stm32f10x.h"
+#include "Configuration.h"
+#include "TaskManager.h"
+#include "USART.h"
+#include "I2C.h"
+#include "Timer.h"
+#include "ADC.h"
+#include "PWM.h"
+#include "flash.h"
+#include "InputCapture_TIM.h"
+#include "InputCapture_EXIT.h"
+#include "LED.h"
+
+//Timer T1(TIM1,1,2,3); //使用定时器计，溢出时间:1S+2毫秒+3微秒
+USART com(1,115200,true);
+//I2C i2c2(2); 
+//PWM pwm2(TIM2,1,1,1,1,20000);  //开启时钟2的4个通道，频率2Whz
+//PWM pwm3(TIM3,1,1,0,0,20000);  //开启时钟3的2个通道，频率2Whz
+//PWM pwm4(TIM4,1,1,1,0,20000);  //开启时钟4的3个通道，频率2Whz
+//InputCapture_TIM t4(TIM4, 400, true, true, true, true);
+//InputCapture_EXIT ch1(GPIOB,6);
+ADC voltage(4); //读取电压值
+//flash InfoStore(0x08000000+100*MEMORY_PAGE_SIZE,true);     //flash
+
+//LED
+GPIO ledRedGPIO(GPIOB,0,GPIO_Mode_Out_PP,GPIO_Speed_50MHz);//LED GPIO
+GPIO ledBlueGPIO(GPIOB,1,GPIO_Mode_Out_PP,GPIO_Speed_50MHz);//LED GPIO
+LED ledRed(ledRedGPIO);//LED red
+LED ledBlue(ledBlueGPIO);//LED blue
+
+
+
+
+
+int main()
+{
+	ledBlue.On();
+	ledRed.Off();
+	double record_tmgTest=0; //taskmanager时间 测试
+	while(1)
+	{
+		if(tskmgr.TimeSlice(record_tmgTest,0.2)) //每0.2秒执行一次
+		{
+			ledRed.Toggle();
+			com<<voltage.Voltage_I(4,5.1,1,12)<<"\n";
+		}
+		
+		ledBlue.Blink(0,0.5,false);
+	}
+}
