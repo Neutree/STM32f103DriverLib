b06250f3adb0da48429c3e190a2f832c9a82c88d
Neucrack <CZD666666@gmail.com>
2018-08-16 18:39:48 +0800

    temp

--- example/onchip/IAP/toolApp/STM32_IAP.py
+++ example/onchip/IAP/toolApp/STM32_IAP.py
@@ -23,7 +23,7 @@ def WriteCOM(com,bytes,ack,timeoutS):
     com.write(bytes)
     time.sleep(0.5)
     timestart = time.time()
-    rcvStr = ""
+    rcvStr = b""
     while 1:
         # length = max(1, min(2048, com.in_waiting))
         # rcv = com.read(length)
@@ -31,7 +31,7 @@ def WriteCOM(com,bytes,ack,timeoutS):
             break
         len = com.in_waiting
         if len:
-            rcv = com.read(len).decode()
+            rcv = com.read(len)
             rcvStr = rcvStr+rcv
         if ack in rcvStr:
             break
@@ -54,6 +54,44 @@ class IAP_USART_Pack:
     data = [0 for i in range(MEMORY_PAGE_SIZE)]
     checkSum = 0x0000
 
+    def calcCheckSum(self):
+        if self.len%2 != 0:
+            print("len % 2 != 0,len:",self.len)
+            return None
+        sum = self.data[1]*256 + self.data[0]
+        for i in range(1,int(self.len/2)):
+            temp = (self.data[i*2+1]*256 + self.data[i*2])
+            sum = sum^temp
+        return sum
+
+    def toString(self):
+        objStr = "\n----------------------------\n"
+        objStr += (("start flag:0x%02X\ntotal opack:"+(str)(self.packTotal)+"\npack ID:"+(str)(self.packID)+"\nlen:"+(str)(self.len)+"\ncheck sum:0x%02X\n") %(self.packStart,self.checkSum))
+        objStr +="data:\n"
+        for i in range(MEMORY_PAGE_SIZE):
+            objStr += ("%02X " %(self.data[i]))
+        objStr += "\n----------------------------\n"
+        return objStr
+    
+    def toBytes(self):
+        ret = []
+        ret.append(self.packStart&0xff)
+        ret.append(self.packStart>>8&0xff)
+        ret.append(self.packStart>>16&0xff)
+        ret.append(self.packStart>>24&0xff)
+        ret.append(self.packTotal&0xff)
+        ret.append(self.packTotal>>8&0xff)
+        ret.append(self.packID&0xff)
+        ret.append(self.packID>>8&0xff)
+        ret.append(self.len&0xff)
+        ret.append(self.len>>8&0xff)
+        for i in self.data:
+            ret.append(i)
+        ret.append(self.checkSum&0xff)
+        ret.append(self.checkSum>>8&0xff)
+        return ret
+
+
 
 #######################
 # main
@@ -75,8 +113,30 @@ com.open()
 
 
 appBin = readBinfile(binFilePath)
-print(appBin)
 
+totalLen = (int)(len(appBin)/MEMORY_PAGE_SIZE)
+if len(appBin) % MEMORY_PAGE_SIZE != 0:
+    totalLen += 1
+pack = IAP_USART_Pack()
+pack.packTotal = totalLen
+print("total pack len:",totalLen)
+
+for i in range(totalLen):
+    pack.packID = i
+    if i+1 != totalLen:
+        pack.len = MEMORY_PAGE_SIZE
+    else:
+        pack.len = len(appBin) % MEMORY_PAGE_SIZE
+    for j in range(pack.len):
+        pack.data[j] = appBin[i*MEMORY_PAGE_SIZE+j]
+    for j in range(pack.len,MEMORY_PAGE_SIZE):
+        pack.data[j] = 0
+    pack.checkSum = pack.calcCheckSum()
+    transBytes = pack.toBytes()
+    print(pack.toString())
+    ackBytes=[0x78,0x3E,0x00,0xAE,pack.packID&0xff,pack.packID>>8&0xff,pack.packTotal&0xff,pack.packTotal>>8&0xff,1,0,0]
+    ...
+    WriteCOM(com,transBytes,ackBytes,1)
 
 
 com.close()
